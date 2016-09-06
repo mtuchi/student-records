8b4c5b4d903ad08df174545cf2f37daebc770f97
mtuchi <mtuchi@bitbucket.com>
2016-09-07 01:04:37 +0300

    Created SubjectController

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SubjectController extends Controller
+{
+    //
+    public function show()
+    {
+      
+    }
+}
