3b360299c1c896411a9a4a3d1ab5fd6fc635e537
Joshua Mabina <mabinajoshua@gmail.com>
2016-09-06 17:17:27 +0300

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+			'email' => 'teacher@gonzaga.ac.tz'
+		]);
+    }
+}
