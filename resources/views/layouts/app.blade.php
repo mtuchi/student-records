df3a1c66f0ea9a0af414f0a757d4dcb802940886
Joshua Mabina <mabinajoshua@gmail.com>
2016-09-06 17:52:49 +0300

    User can log in and out.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,11 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+						@if (Route::has('login'))
+							{{-- <li><a href="{{ url('/register') }}">Register</a></li> --}}
+						@else
+	                        <li><a href="{{ url('/login') }}">Login</a></li>
+						@endif
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
