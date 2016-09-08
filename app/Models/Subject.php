f9f49a9c7da6596297c2a4cba7574eebdd40952a
Joshua Mabina <mabinajoshua@gmail.com>
2016-09-08 09:10:12 +0300

    Fix OCD

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = [
-		'name'
+		'name',
 	];
 }
