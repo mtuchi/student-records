270398771526658ec4d3783b27dbf2ac062b1961
Joshua Mabina <mabinajoshua@gmail.com>
2016-09-08 08:00:06 +0300

    Add maatwebsite/excel package for importing and exporting Excel and CSV files.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Facades\Excel;
 use App\Models\Subject;
 
 class HomeController extends Controller
@@ -48,7 +49,8 @@ class HomeController extends Controller
     {
 		$file = $request->file('sheet');
 
-		dd($file->path());
+		$rowCollection = Excel::load($file)->get();
+		dd($rowCollection);
 
 		return redirect('/home');
     }
