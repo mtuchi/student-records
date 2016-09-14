b4f0dee5c0f1911ced7461a8235087630fba1008
mtuchi <emmanuel@irabu.co.tz>
2016-09-15 02:44:13 +0300

    Updated QuatersTableSeeder to input slug as well

--- database/seeds/QuatersTableSeeder.php
+++ database/seeds/QuatersTableSeeder.php
@@ -15,22 +15,27 @@ class QuatersTableSeeder extends Seeder
         [
           'name' => 'First Quater',
           'live' => true,
+          'slug' =>'first_quater'
         ],
         [
           'name' => 'Second Quater',
           'live' => true,
+          'slug' =>'second_quater'
         ],
         [
           'name' => 'Third Quater',
           'live' => true,
+          'slug' =>'third_quater'
+
         ],
         [
           'name' => 'Fourth Quater',
           'live' => false,
+          'slug' =>'fourth_quater'
         ],
       ];
       foreach ($arr as $k => $v) {
-        factory(Quater::class)->create(['name' => $v['name'], 'live'=>$v['live']]);
+        factory(Quater::class)->create(['name' => $v['name'], 'live'=>$v['live'], 'slug'=>$v['slug']]);
       }
     }
 }
