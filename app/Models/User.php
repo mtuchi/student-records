88f9465fbad05733412c17e4a69e318fae028f82
mtuchi <emmanuel@irabu.co.tz>
2016-09-15 02:49:02 +0300

    Modified User Model although am not sure if its working

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Subject;
+use App\Models\Quater;
 
 class User extends Authenticatable
 {
@@ -30,6 +31,9 @@ class User extends Authenticatable
 
     public function subjects() {
         return $this->hasMany(Subject::class);
+    }
+    public function quaters() {
+        return $this->hasMany(Quater::class);
     }
 
 }
