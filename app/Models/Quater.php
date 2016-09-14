31b1ca85ca1d9ad479af5c89f48ad36bc3db66d7
mtuchi <emmanuel@irabu.co.tz>
2016-09-15 02:48:23 +0300

    Modefied Quater Model

--- app/Models/Quater.php
+++ app/Models/Quater.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Quater extends Model
 {
   protected $fillable = ['name','live'];
-
+  
+  public function scopeIsLive($query)
+  {
+    return $query->where('live', true);
+  }
 }
