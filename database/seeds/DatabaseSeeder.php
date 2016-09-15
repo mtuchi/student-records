ad269039be111f00554292db749c7caf57d6a101
mtuchi <emmanuel@irabu.co.tz>
2016-09-15 23:43:30 +0300

    Added Score Seeder in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(StudentTableSeeder::class);
         $this->call(SubjectsTableSeeder::class);
         $this->call(QuartersTableSeeder::class);
+        $this->call(ScoreTableSeeder::class);
     }
 }
---
+++ database/seeds/StudentTableSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\Student;
+use Illuminate\Database\Seeder;
+
+class StudentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $arr = [
+        [
+          'name' => 'Cameron, Douglas',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Cannon, John',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Card, J. C',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Bond, Jesse',
+          'gender' => 'f',
+        ],
+        [
+          'name' => 'Bösenhofer, Markus',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Buckley, Robert',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Edwards, David',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Fredenslund, Aage',
+          'gender' => 'm',
+        ],
+        [
+          'name' => 'Eiseman, Mike',
+          'gender' => 'f',
+        ],
+        [
+          'name' => 'Elgohary, Amal',
+          'gender' => 'f',
+        ],
+      ];
+
+      foreach ($arr as $k => $v) {
+        factory(Student::class)->create(['name' => $v['name'], 'gender'=>$v['gender']]);
+      }
+    }
+}
