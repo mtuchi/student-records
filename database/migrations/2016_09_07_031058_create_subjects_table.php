c46536d645a69b402582c972f420d5ed95df8567
mtuchi <emmanuel@irabu.co.tz>
2016-09-15 23:44:44 +0300

    updated subject and student migrations

--- database/migrations/2016_09_07_031058_create_subjects_table.php
+++ database/migrations/2016_09_07_031058_create_subjects_table.php
@@ -17,7 +17,7 @@ class CreateSubjectsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('user_id')->unsigned()->nullable();
-			$table->foreign('user_id')->references('id')->on('users');
+			      $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
--- database/migrations/2016_09_08_061231_create_students_table.php
+++ database/migrations/2016_09_08_061231_create_students_table.php
@@ -15,8 +15,8 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('name');
-			$table->enum('gender', ['f', 'm']);
+      			$table->string('name');
+      			$table->enum('gender', ['f', 'm']);
             $table->timestamps();
         });
     }
