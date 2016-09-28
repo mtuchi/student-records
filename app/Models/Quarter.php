5d50e275691ced9a3198dcad9048fac0d4a289b7
mtuchi <emmanuel@irabu.co.tz>
2016-09-28 14:16:54 +0300

    added score relationship for subjects

--- app/Models/Quarter.php
+++ app/Models/Quarter.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Score;
 use App\Models\Subject;
+use Request;
 
 class Quarter extends Model
 {
@@ -18,7 +19,7 @@ class Quarter extends Model
 
   public function score()
   {
-    return $this->hasMany('App\Models\Score', 'quarter_id');
+    return $this->hasMany(Score::class)->where('subject_id', Request::route()->subject->id);
   }
 
 }
