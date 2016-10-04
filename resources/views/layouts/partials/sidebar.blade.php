dc5945c2d3798aff87fe2fe71e5f1d29adeefed6
mtuchi <emmanuel@irabu.co.tz>
2016-10-04 20:45:41 +0300

    Updated views, now each view can be displayed depending on the user role

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,5 +1,6 @@
 <div class="panel panel-default">
-    <div class="panel-heading">Sections</div>
+  @if(Auth::user()->hasRole('teacher'))
+    <div class="panel-heading">Teacher Sections</div>
     <div class="panel-body">
       <div class="boxed-group-inner">
         Class Assigned with subject
@@ -10,4 +11,18 @@
         </ul>
       </div>
     </div>
+  @endif
+  @if(Auth::user()->hasRole('class_teacher'))
+    <div class="panel-heading">Class Teacher Sections</div>
+    <div class="panel-body">
+      <div class="boxed-group-inner">
+        Class Assigned with subject
+        <ul class="mini-class-list js-class-list">
+          @foreach($subjects as $subject)
+            <li><a href="{{ route('user.subject',$subject->slug ) }}">{{ $subject->name }} {{ $subject->class }}</a></li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+  @endif
 </div>
