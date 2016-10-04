26221da208f6a1512469089968ec7d507259f122
mtuchi <emmanuel@irabu.co.tz>
2016-10-04 20:44:22 +0300

    Added UserRole relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,8 +29,75 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function subjects() {
+    public function subjects()
+    {
         return $this->hasMany(Subject::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    /**
+    * Find out if User is a teacher, based on if has any roles     *
+      * @return boolean
+    */
+    public function isTeacher()
+    {
+         $roles = $this->roles->toArray();
+         return !empty($roles);
+    }
+
+    /**
+    * Find out if user has a specific role
+    *
+    * $return boolean
+    */
+    public function hasRole($role)
+    {
+        $roles = collect($this->roles->pluck('name')->toArray());
+        return $roles->contains($role);
+    }
+
+    /**
+    * Get key in array with corresponding value
+    *
+    * @return int
+    */
+    private function getIdInArray($array, $term)
+    {
+       foreach ($array as $key => $value) {
+           if ($value['name'] == $term) {
+               return $key;
+           }
+       }
+
+       throw new UnexpectedValueException;
+    }
+
+    /**
+    * Add roles to user to make them a teacher
+    */
+    public function makeTeacher($title)
+    {
+        $assigned_roles = [];
+        $roles = Role::all()->keyBy('id')->toArray();
+
+       switch ($title)
+       {
+           case 'class_teacher':
+               $assigned_roles[] = $this->getIdInArray($roles, 'class_teacher');
+               break;
+
+           case 'teacher':
+               $assigned_roles[] = $this->getIdInArray($roles, 'teacher');
+               break;
+           default:
+               throw new \Exception("The teacher status entered does not exist");
+       }
+
+       $this->roles()->attach($assigned_roles);
     }
 
 }
