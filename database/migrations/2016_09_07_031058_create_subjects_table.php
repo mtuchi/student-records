fe87884e37cce25ef4d83e4e3ca24cc2faeef710
mtuchi <emmanuel@irabu.co.tz>
2016-10-10 17:07:20 +0300

    Created pivot table for grade and user, also teachers table migrations

--- database/migrations/2016_09_07_031058_create_subjects_table.php
+++ database/migrations/2016_09_07_031058_create_subjects_table.php
@@ -16,8 +16,6 @@ class CreateSubjectsTable extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('user_id')->unsigned()->nullable();
-			      $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
