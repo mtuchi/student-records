16e1649695b934a8cbe2d0a3ab8d12357f6795a4
mtuchi <emmanuel@irabu.co.tz>
2016-11-08 23:03:55 +0300

    re-added vue package

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const elixir = require('laravel-elixir');
 
+require('laravel-elixir-vue-2');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
