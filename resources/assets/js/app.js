65bcc6c6e16f7af1539169c2ea7877e986b1df94
mtuchi <emmanuel@irabu.co.tz>
2016-11-08 23:04:51 +0300

    Created export component and example component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,3 +3,24 @@ require('./bootstrap');
 
 // Require DataTables for jQuery with styling for Bootstrap
 // require('datatables.net-bs');
+
+/*
+  Require dropdown
+  source:http://behigh.github.io/bootstrap_dropdowns_enhancement/
+*/
+  require('../bower_components/dropdown/dropdowns-enhancement');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the body of the page. From here, you may begin adding components to
+ * the application, or feel free to tweak this setup for your needs.
+ */
+  var VueResource = require('vue-resource');
+
+  Vue.component('export', require('./components/Export.vue'));
+  Vue.component('example', require('./components/Example.vue'));
+  Vue.use(VueResource);
+
+  const app = new Vue({
+      el: '#app'
+  });
