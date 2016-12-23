5df090901284e7812299d67909ab76844674c72f
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2016-12-23 21:08:53 +0300

    updated Student seeder to use factory

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -12,52 +12,6 @@ class StudentsTableSeeder extends Seeder
      */
     public function run()
     {
-      $arr = [
-        [
-          'name' => 'Cameron, Douglas',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Cannon, John',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Card, J. C',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Bond, Jesse',
-          'gender' => 'f',
-        ],
-        [
-          'name' => 'Bösenhofer, Markus',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Buckley, Robert',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Edwards, David',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Fredenslund, Aage',
-          'gender' => 'm',
-        ],
-        [
-          'name' => 'Eiseman, Mike',
-          'gender' => 'f',
-        ],
-        [
-          'name' => 'Elgohary, Amal',
-          'gender' => 'f',
-        ],
-      ];
-
-      foreach ($arr as $k => $v)
-      {
-        factory(Student::class)->create(['name' => $v['name'], 'gender'=>$v['gender']]);
-      }
+			factory(Student::class,10)->create();
     }
 }
