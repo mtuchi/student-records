bde06bedb6f45b80e79c19e67a3b53f2d2ebc8b2
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2016-12-27 21:15:52 +0300

    added assign method to StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\Models\Activity;
 
 use App\Http\Requests;
+use App\Http\Requests\EditStudentFormRequest;
 
 use App\Models\Grade;
 use App\Models\Student;
@@ -42,7 +43,7 @@ class StudentController extends Controller
 
 		public function edit($id)
 		{
-			$student = Student::where('id', $id)->firstOrFail();
+			$student = Student::where('id', $id)->with('grade')->firstOrFail();
 			return view('student.edit',['student' => $student,'id' => $id]);
 		}
 
@@ -50,5 +51,136 @@ class StudentController extends Controller
 		{
 			$student = Student::where('id', $id)->firstOrFail();
 			return view('student.show',['student' => $student,'id' => $id]);
+		}
+
+		public function store(EditStudentFormRequest $request)
+		{
+			$student = Student::create([
+				'name' => $request->name,
+				'guardian' => $request->guardian,
+				'emergency_contact' => $request->phone,
+				'gender' => $request->gender,
+				'dob' => $request->dob,
+			]);
+
+			activity('created-student')
+			->causedBy(Auth::user())
+			->performedOn($student)
+			->withProperties([
+			'attributes' => [
+				'name' => $request->name,
+				'guardian' => $request->guardian,
+				'emergency_contact' => $request->phone,
+				'gender' => $request->gender,
+				'dob' => $request->dob,
+			],
+			'type' => 'success',
+			])
+			->log($student->name." Records has been created successful by ". Auth::user()->name);
+
+			notify()->flash($request->name." has been added", 'success');
+
+			return redirect('students');
+		}
+
+		public function assign(Request $request, $id)
+		{
+			$slug = $request->class."-".$request->stream;
+			$name = $request->class." ".$request->stream;
+
+			$student = Student::find($id);
+
+			$grade = Grade::where('slug', $slug)->first();
+
+			if ($grade) {
+				if ($student->grade->count()) {
+
+					$student->grade()->sync([$grade->id => [ 'student_id' => $id] ], false);
+					// $student->grade()->updateExistingPivot($grade->id,['student_id' => $id]);
+
+					notify()->flash($student->name." has been assigned to $grade->name ", 'success');
+
+						// foreach ($student->grade as $grade) {
+						// 	notify()->flash($student->name." is already assigned to $grade->name ", 'warning');
+						// }
+		 		  return redirect('students');
+
+				} else {
+					$student->grade()->attach($grade->id);
+
+					notify()->flash($student->name." has been assigned to $grade->name ", 'success');
+		 		  return redirect('students');
+				}
+			} else {
+				notify()->flash("The selected class is not assigned to a class teacher yet, choose anothe class", 'danger');
+				return redirect('students/'.$id.'/edit')
+								->withInput();
+			}
+		}
+
+		public function update($id, EditStudentFormRequest $request)
+		{
+			$student = Student::where('id', $id)->first();
+
+			activity('update-teacher')
+			->causedBy(Auth::user())
+			->performedOn($student)
+			->withProperties([
+			'attributes' => [
+				'name' => $request->name,
+				'guardian' => $request->guardian,
+				'emergency_contact' => $request->phone,
+				'gender' => $request->gender,
+				'dob' => $request->dob,
+			],
+			'old' => [
+				'name' => $student->name,
+				'guardian' => $student->guardian,
+				'emergency_contact' => $student->phone,
+				'gender' => $student->gender,
+				'dob' => $student->dob,
+			],
+			'type' => 'success',
+			])
+			->log($student->name." Records has been updated successful by ". Auth::user()->name);
+
+			$student->update([
+				'name' => $request->name,
+				'guardian' => $request->guardian,
+				'emergency_contact' => $request->phone,
+				'gender' => $request->gender,
+				'dob' => $request->dob,
+		 ]);
+
+		 notify()->flash($student->name." has been updated", 'success');
+
+		 return redirect('students');
+		}
+
+		public function destroy($id)
+		{
+			$student = Student::find($id);
+			$student->delete();
+
+			$student->onlyTrashed()->where('id', $id)->first();
+
+			activity('deleted-student')
+			->causedBy(Auth::user())
+			->performedOn($student)
+			->withProperties([
+			'attributes' => [
+				'name' => $student->name,
+				'guardian' => $student->guardian,
+				'emergency_contact' => $student->phone,
+				'gender' => $student->gender,
+				'dob' => $student->dob,
+			],
+			'type' => 'success',
+			])
+			->log($student->name." Records has been deleted successful by ". Auth::user()->name);
+
+			notify()->flash($student->name. " record deleted", 'success');
+
+			return redirect('students');
 		}
 }
