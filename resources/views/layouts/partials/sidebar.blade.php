16c6bb21a7255471a2cc6015172873988249d9ed
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2017-01-03 12:49:34 +0300

    modified class_teacher role condition to recieve data by elequent relation

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -22,10 +22,9 @@
       <div class="boxed-group-inner">
         Class Assigned with subject
         <ul class="mini-class-list js-class-list">
-          @foreach($subjects as $subject)
-            @foreach($subject->grade as $grade)
-              <li><a href="{{ route('quarter.show',[$grade->slug,$subject->subject->name]) }}">{{$subject->subject->name}}</a></li>
-            @endforeach
+          @foreach($teachers as $teacher)
+						{{-- {{ dd($teacher) }} --}}
+						{{-- <li><a href="{{ route('quarter.show',[$teacher->grade->slug,$teacher->subject->name]) }}">{{ $teacher->subject->name }}</a></li> --}}
           @endforeach
         </ul>
       </div>
@@ -40,27 +39,35 @@
         <div class="boxed-group-inner">
           Class Attendance for
           <ul class="mini-class-list js-class-list">
-            <li><a href="{{ route('grade.show',$getgrade->slug) }}">{{ $getgrade->name }}</a></li>
+            <li><a href="{{ route('grade.show',$grade->slug) }}">{{ $grade->name }}</a></li>
           </ul>
         </div>
       </div>
     </div>
     <div class="panel panel-default">
-      <div class="panel-heading">@ {{ $getgrade->name  }} Class, Teachers List</div>
+      <div class="panel-heading">@ {{ $grade->name  }} Class, Teachers List</div>
       <div class="panel-body">
         <div class="boxed-group-inner">
           Teachers with their assaigned subjects
           <ul class="mini-class-list js-class-list">
-            @foreach($teachers as $teacher)
+            @foreach($grade->teacher as $teacher)
               <li class="col-xs-12 col-sm-8 col-md-12">
                 <svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12">
                 <path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
                 <span class="author" itemprop="author">
-                <a href="#" class="url link" rel="author">{{$teacher->teacher->username}}</a>
+									@if ($teacher->user)
+										<a href="#" class="url link" rel="author">{{$teacher->user->username}}</a>
+										@else
+										<span class="badge" title="No Assigned Subject Teacher Yet!" data-toggle="tooltip" data-placement="top">N A</span>
+									@endif
                 </span>
                 <span class="path-divider">/</span>
                 <strong>
-                <a href="#">{{ $teacher->subject->name }} </a>
+									@if ($teacher->subject)
+										<a href="#">{{ $teacher->subject->name }} </a>
+										@else
+											<span class="badge" title="No Assigned Subject Yet!" data-toggle="tooltip" data-placement="top">N A</span>
+									@endif
                 </strong>
               </li>
             @endforeach
