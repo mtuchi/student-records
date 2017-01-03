88e83b3795d7fdfbf1db92cdbb1ae0daab6fc5b7
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2017-01-03 12:53:20 +0300

    updated user, subject ,student relation

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -13,6 +13,21 @@ class Grade extends Model
       return $this->belongsTo(User::class,'user_id');
     }
 
+		public function teacher()
+		{
+			return $this->hasMany(Teacher::class);
+		}
+
+		public function subject()
+		{
+			return $this->belongsToMany(Subject::class);
+		}
+
+		public function student()
+		{
+			return $this->belongsToMany(Student::class);
+		}
+
     public function quarter()
     {
       return $this->hasMany(Quarter::class);
