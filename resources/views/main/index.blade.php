f4222905c883eaf43d795b324c05881b480dc71b
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2017-01-06 12:34:35 +0300

    update the welcome message

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,63 +1,52 @@
 @extends('home')
 
 @section('main.content')
-
-		@if(Auth::user()->hasRole('admin'))
-			<div class="panel panel-default">
-				<div class="panel-heading">Dashboard</div>
-					<div class="panel-body">
-					<div class="dashboard-notice js-notice">
-						<h2>Welcome to the <strong>Gonzaga</strong> Records Manager!</h2>
-						<p>Here are some quick tips for a <strong>School Admin</strong> member.</p>
-						<ul>
-							<li>
-								Use the link on your right panel to switch to your subject context.
-							</li>
-							<li>
-								After you switch context you'll see student scores dashboard that lists out
-								student score per quarter.
-							</li>
-							<li>
-								To upload students scores, First download the spreedsheet schema,
-								i.e The schema will contain student names and quarter fieldset that you will have to fill
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		@endif
-		@if(Auth::user()->hasRole('teacher'))
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<div class="dashboard-notice js-notice">
-						<h2>Welcome to the <strong>Gonzaga</strong> Records Manager!</h2>
-						<p>Here are some quick tips for a first-time teacher member.</p>
-						<ul>
-							<li>
-								Use the link on your right panel to switch to your subject context.
-							</li>
-							<li>
-								After you switch context you'll see student scores dashboard that lists out
-								student score per quarter.
-							</li>
-							<li>
-								To upload students scores, First download the spreedsheet schema,
-								i.e The schema will contain student names and quarter fieldset that you will have to fill
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		@endif
-		@if(Auth::user()->hasRole('class_teacher'))
-			<div class="panel panel-default">
+	@if(Auth::user()->hasRole('admin'))
+		<div class="panel panel-default">
+			<div class="panel-heading">Welcome Admin</div>
 				<div class="panel-body">
 				<div class="dashboard-notice js-notice">
-					{{-- @foreach($subjects as $subject)
-						{{ dd($subject->name) }}
-					@endforeach --}}
+					<h2>Welcome school adminstrator</h2>
+					<p>Here are some quick tips for a <strong>School Admin</strong> member.</p>
+					<ul>
+						<li>
+							Use the link on your right panel to switch to your subject context.
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>
-		@endif
+	@endif
+	@if(Auth::user()->hasRole('teacher'))
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="dashboard-notice js-notice">
+					<h2>Welcome to the <strong>Gonzaga</strong> Records Manager!</h2>
+					<p>Here are some quick tips for a first-time teacher member.</p>
+					<ul>
+						<li>
+							Use the link on your right panel to switch to your subject context.
+						</li>
+						<li>
+							After you switch context you'll see student scores dashboard that lists out
+							student score per quarter.
+						</li>
+						<li>
+							To upload students scores, First download the spreedsheet schema,
+							i.e The schema will contain student names and quarter fieldset that you will have to fill
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	@endif
+	@if(Auth::user()->hasRole('class_teacher'))
+		<div class="panel panel-default">
+			<div class="panel-body">
+			<div class="dashboard-notice js-notice">
+				<h2>Class teacher role guidance.</h2>
+			</div>
+		</div>
+	</div>
+	@endif
 @endsection
