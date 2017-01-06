d741f2ff2c905bc7ecab788044257af75b1db7a9
Emmanuel Mtuchi <emmanuel@irabu.co.tz>
2017-01-06 12:36:40 +0300

    changed the name for the grade and stream to class

--- resources/views/layouts/partials/_gradelist.blade.php
+++ resources/views/layouts/partials/_gradelist.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-8 form-inline">
-  <select class="form-control" name="class" placeholder="Select Class" required>
+  <select class="form-control" name="class[0]" placeholder="Select Class" required>
     <option value="">Select Class</option>
 		<option value="Pre">Pre</option>
     <option value="I">I</option>
@@ -10,19 +10,14 @@
     <option value="VI">VI</option>
     <option value="VII">VII</option>
   </select>
-  @if ($errors->has('class'))
-      <span class="help-block">
-          <strong>{{ $errors->first('class') }}</strong>
-      </span>
-  @endif
-  <select class="form-control" name="stream" placeholder="Select Stream">
+  <select class="form-control" name="class[1]" placeholder="Select Stream">
     <option value="">Select Stream</option>
     <option value="A">A</option>
     <option value="B">B</option>
   </select>
-  @if ($errors->has('stream'))
-      <span class="help-block">
-          <strong>{{ $errors->first('stream') }}</strong>
-      </span>
-  @endif
 </div>
+@if ($errors->has('class'))
+		<span class="help-block">
+				<strong>{{ $errors->first('class') }}</strong>
+		</span>
+@endif
