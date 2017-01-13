5273ce9a42a46e4b6f55e36ecb9356c2d42a8d91
mtuchi <emmanuel@irabu.co.tz>
2017-01-13 19:01:29 +0300

    updated class_teacher tabs

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -34,51 +34,51 @@
 
   @if(Auth::user()->hasRole('class_teacher'))
   <div class="panel with-nav-tabs panel-default">
-	  <div class="panel-heading">
-		  <ul class="nav nav-tabs">
-			  <li class="active"><a href="#class_section" data-toggle="tab">Class Section</a></li>
-			  <li><a href="#teachers" data-toggle="tab">Teachers</a></li>
-		  </ul>
-	  </div>
-	  <div class="panel-body">
-		  <div class="tab-content">
-			  <div class="tab-pane fade in active" id="class_section">
-				  <div class="boxed-group-inner">
-		            <ul class="mini-class-list js-class-list">
-		  			 {{ $grade->slug }} profile
-		              <li><a href="{{ route('grade.show',$grade->slug) }}">{{ $grade->slug }}</a></li>
-		  			Class Attendance
-		  			<li><a href="#">Attendance for {{ $grade->slug }}</a></li>
-		            </ul>
-		          </div>
-			  </div>
-			  <div class="tab-pane fade" id="teachers">
-				  <div class="boxed-group-inner">
-		            Teachers with their assaigned subjects
-		            <ul class="mini-class-list js-class-list">
-		              @foreach($grade->teacher as $teacher)
-		  				<li class="col-xs-12 col-sm-8 col-md-12">
-		  					<svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12">
-		  					<path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
-		  					<span class="author" itemprop="author">
-		  						@if ($teacher->user)
-		  							<a href="#" class="url link" rel="author">{{$teacher->user->username}}</a>
-		  							@else
-		  							<span class="badge" title="No Assigned Subject Teacher Yet!" data-toggle="tooltip" data-placement="top">N A</span>
-		  						@endif
-		  					</span>
-		  					<span class="path-divider">/</span>
-		  					<strong>
-		  						@if ($teacher->subject)
-		  							<a href="#">{{ $teacher->subject->name }} </a>
-		  							@else
-		  							<span class="badge" title="No Assigned Subject Yet!" data-toggle="tooltip" data-placement="top">N A</span>
-		  						@endif
-		  					</strong>
-		  				</li>
-		              @endforeach
-		            </ul>
-		          </div>
+	  <div class="tabbable-panel">
+		  <div class="tabbable-line">
+			  <ul class="nav nav-tabs nav-justified">
+				  <li class="active"><a href="#class_section" data-toggle="tab">Class Section</a></li>
+				  <li><a href="#teachers" data-toggle="tab">Teachers</a></li>
+			  </ul>
+			  <div class="tab-content">
+				  <div class="tab-pane active" id="class_section">
+					  <div class="boxed-group-inner">
+			            <ul class="mini-class-list js-class-list">
+			  			 {{ $grade->slug }} profile
+			              <li><a href="{{ route('grade.show',$grade->slug) }}">{{ $grade->slug }}</a></li>
+			  			Class Attendance
+			  			<li><a href="#">Attendance for {{ $grade->slug }}</a></li>
+			            </ul>
+			          </div>
+				  </div>
+				  <div class="tab-pane" id="teachers">
+					  <div class="boxed-group-inner container">
+			            Teachers with their assaigned subjects
+			            <ul class="mini-class-list js-class-list">
+			              @foreach($grade->teacher as $teacher)
+			  				<li class="col-xs-12 col-sm-8 col-md-12">
+			  					<svg aria-hidden="true" class="octicon octicon-repo" height="16" version="1.1" viewBox="0 0 12 16" width="12">
+			  					<path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+			  					<span class="author" itemprop="author">
+			  						@if ($teacher->user)
+			  							<a href="#" class="url link" rel="author">{{$teacher->user->username}}</a>
+			  							@else
+			  							<span class="badge" title="No Assigned Subject Teacher Yet!" data-toggle="tooltip" data-placement="top">N A</span>
+			  						@endif
+			  					</span>
+			  					<span class="path-divider">/</span>
+			  					<strong>
+			  						@if ($teacher->subject)
+			  							<a href="#">{{ $teacher->subject->name }} </a>
+			  							@else
+			  							<span class="badge" title="No Assigned Subject Yet!" data-toggle="tooltip" data-placement="top">N A</span>
+			  						@endif
+			  					</strong>
+			  				</li>
+			              @endforeach
+			            </ul>
+			          </div>
+				  </div>
 			  </div>
 		  </div>
 	  </div>
