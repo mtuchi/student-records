bfbf54691e1f8e524ed37a07912c0ec755c4a4bc
mtuchi <emmanuel@irabu.co.tz>
2017-01-16 00:32:19 +0300

    modified a sample report card

--- resources/views/settings/print/sample.blade.php
+++ resources/views/settings/print/sample.blade.php
@@ -4,63 +4,257 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-lg-12">
-			<table class="table table-bordered">
-				<thead>
-					<tr role="row">
-						<th class="text-center" rowspan="1" colspan="10" style="vertical-align: middle;">Academic Progress Report</th>
-						<th rowspan="1">
-							<img src="http://s3.amazonaws.com/s3.codecourse.com/public/img/email/logo.png" alt="Codecourse logo" class="pull-">
-						</th>
-					</tr>
-				    <tr role="row">
-				        <th colspan="1" rowspan="2" style="vertical-align: middle;">Pupil's Name</th>
-				        <th colspan="8" rowspan="2" style="vertical-align: middle;">Jaden Smith Maeda</th>
-				        <th colspan="2" rowspan="1">Pre-Standard I A</th>
-				    </tr>
+		<div class="col-lg-8 col-md-8">
+			<div class="row">
+				<table class="table table-bordered">
+					<thead>
+						<tr role="row">
+							<th class="text-center" rowspan="1" colspan="10" style="vertical-align: middle;">Academic Progress Report</th>
+							<th rowspan="1">
+								<img src="http://s3.amazonaws.com/s3.codecourse.com/public/img/email/logo.png" alt="Codecourse logo" class="pull-">
+							</th>
+						</tr>
+						<tr role="row">
+							<th colspan="1" rowspan="2" style="vertical-align: middle;">Pupil's Name</th>
+							<th colspan="8" rowspan="2" style="vertical-align: middle;">Jaden Smith Maeda</th>
+							<th colspan="2" rowspan="1">Pre-Standard I A</th>
+						</tr>
 
-					<tr>
-						<th rowspan="1">Absences:</th>
-						<th rowspan="1">N/A</th>
-					</tr>
+						<tr>
+							<th rowspan="1">Absences:</th>
+							<th rowspan="1">N/A</th>
+						</tr>
 
-					<tr role="row">
-				        <th class="sorting" rowspan="1">Subject</th>
-				        <th class="sorting" rowspan="1" colspan="3">Teacher</th>
-				        <th class="sorting" rowspan="1">Q.1</th>
-				        <th class="sorting" rowspan="1">Q.2</th>
-						<th class="sorting" rowspan="1">Q.3</th>
-						<th class="sorting" rowspan="1">Q.4</th>
-				        <th class="sorting" rowspan="1">Term 1</th>
-						<th class="sorting" rowspan="1">Term 2</th>
-						<th class="sorting" rowspan="1">Cumm Avg.</th>
-				    </tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td rowspan="1">English</td>
-						<td rowspan="1" colspan="3">Jane Doe</td>
-						<td rowspan="1">98</td>
-						<td rowspan="1">79</td>
-						<td rowspan="1">89</td>
-						<td rowspan="1">91</td>
-						<td rowspan="1">12</td>
-						<td rowspan="1">12</td>
-						<td rowspan="1">54</td>
-					</tr>
-					<tr>
-						<td rowspan="1">Overall Average</td>
-						<td rowspan="1" colspan="3"></td>
-						<td rowspan="1">98</td>
-						<td rowspan="1">79</td>
-						<td rowspan="1">89</td>
-						<td rowspan="1">91</td>
-						<td rowspan="1">12</td>
-						<td rowspan="1">12</td>
-						<td rowspan="1">54</td>
-					</tr>
-				</tbody>
-			</table>
+						<tr role="row">
+							<th class="sorting" rowspan="1">Subject</th>
+							<th class="sorting" rowspan="1" colspan="3">Teacher</th>
+							<th class="sorting" rowspan="1">Q.1</th>
+							<th class="sorting" rowspan="1">Q.2</th>
+							<th class="sorting" rowspan="1">Q.3</th>
+							<th class="sorting" rowspan="1">Q.4</th>
+							<th class="sorting" rowspan="1">Term 1</th>
+							<th class="sorting" rowspan="1">Term 2</th>
+							<th class="sorting" rowspan="1">Cumm Avg.</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td rowspan="1">English</td>
+							<td rowspan="1" colspan="3">Jane Doe</td>
+							<td rowspan="1">98</td>
+							<td rowspan="1">79</td>
+							<td rowspan="1">89</td>
+							<td rowspan="1">91</td>
+							<td rowspan="1">12</td>
+							<td rowspan="1">12</td>
+							<td rowspan="1">54</td>
+						</tr>
+						<tr>
+							<td rowspan="1">Mathematics</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+						<tr>
+							<td rowspan="1">Kiswahili</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+						<tr>
+							<td rowspan="1">Science</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+						<tr>
+							<td rowspan="1">Arts</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+						<tr>
+							<td rowspan="1">Sports</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+						<tr>
+							<td rowspan="1">General Manner</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+							<td rowspan="1"></td>
+						</tr>
+
+						<tr>
+							<td rowspan="1">Overall Average</td>
+							<td rowspan="1" colspan="3"></td>
+							<td rowspan="1">98</td>
+							<td rowspan="1">79</td>
+							<td rowspan="1">89</td>
+							<td rowspan="1">91</td>
+							<td rowspan="1">12</td>
+							<td rowspan="1">12</td>
+							<td rowspan="1">54</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="row">
+				<table class="table table-bordered">
+					<tbody>
+						<tr>
+							<td colspan="4">Student's Conduct: <hr><hr></td>
+						</tr>
+						<tr>
+							<td colspan="4" rowspan="1">Class Teacher's Overall Assessment:<hr><hr><hr></td>
+						</tr>
+						<tr>
+							<td colspan="2" rowspan="1">Class Teacher's Name: <hr></td>
+							<td colspan="2" rowspan="1">Class Teacher's Signature: <hr></td>
+						</tr>
+						<tr>
+							<td colspan="2" rowspan="1">Head Teacher's Name:<hr></td>
+							<td colspan="2" rowspan="1">School Stamp: <hr></td>
+						</tr>
+						<tr>
+							<td colspan="2" rowspan="1">Parent's Signature: <hr></td>
+							<td colspan="2" rowspan="1">Date: <hr></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4">
+					<table class="table table-bordered col-md-6">
+						<thead>
+							<th class="text-center">Cum. Academic </th>
+						</thead>
+						<tbody>
+							<tr>
+								<td class="text-center">A</td>
+							</tr>
+						</tbody>
+					</table>
+					<table class="table table-bordered col-md-6">
+						<thead>
+							<th class="text-center">Conduct Key </th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>&nbsp;</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="col-md-8">
+					<div class="col-md-6">
+						<table class="table table-bordered">
+							<thead>
+								<th colspan="3" class="text-center">Academic Key</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Excellent</td>
+									<td>81-100</td>
+									<td>A</td>
+								</tr>
+								<tr>
+									<td>Good</td>
+									<td>61-80</td>
+									<td>B</td>
+								</tr>
+								<tr>
+									<td>Satisfactory</td>
+									<td>41-60</td>
+									<td>C</td>
+								</tr>
+								<tr>
+									<td>Needs Improvement</td>
+									<td>21-40</td>
+									<td>D</td>
+								</tr>
+								<tr>
+									<td>Falling</td>
+									<td>0-20</td>
+									<td>F</td>
+								</tr>
+								<tr>
+									<td>Promoted</td>
+									<td></td>
+									<td>P</td>
+								</tr>
+								<tr>
+									<td>Advised to Repeat</td>
+									<td></td>
+									<td>R</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div class="col-md-6">
+						<table class="table table-bordered">
+							<thead>
+								<th colspan="2" class="text-center">Conduct Key</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Outstanding</td>
+									<td>O</td>
+								</tr>
+								<tr>
+									<td>Very Good</td>
+									<td>VG</td>
+								</tr>
+								<tr>
+									<td>Average</td>
+									<td>A</td>
+								</tr>
+								<tr>
+									<td>Needs Improvement</td>
+									<td>NI</td>
+								</tr>
+								<tr>
+									<td>Disruptive</td>
+									<td>D</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </div>
