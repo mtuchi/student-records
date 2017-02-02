d23d4ee9dd048583cfddc00b77e83e98ecd5f26d
Emmanuel Evance <emmanuel@irabu.co.tz>
2017-02-02 15:43:56 +0300

    removed profile link in _navigation menu

--- resources/views/layouts/partials/_navigations.blade.php
+++ resources/views/layouts/partials/_navigations.blade.php
@@ -33,30 +33,26 @@
                   @endif
                 @else
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
 
-                        <ul class="dropdown-menu" role="menu">
-                          <li class="dropdown-header">Signed in as {{ Auth::user()->name }}</li>
-                          <li role="separator" class="divider"></li>
-                          <li><a href="{{ route('user.show', Auth::user()->username )}}">Profile</a></li>
-                          <li>
-                            <a href="">Settings</a>
-                          </li>
+                      <ul class="dropdown-menu" role="menu">
+                        <li class="dropdown-header">Signed in as {{ Auth::user()->name }}</li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="{{ route('user.show', Auth::user()->username )}}">Settings</a></li>
+                        <li>
+                          <a href="{{ url('/logout') }}"
+                              onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                              Logout
+                          </a>
 
-                          <li>
-                              <a href="{{ url('/logout') }}"
-                                  onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                                  Logout
-                              </a>
-
-                              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                  {{ csrf_field() }}
-                              </form>
-                          </li>
-                        </ul>
+                          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                          </form>
+                        </li>
+                      </ul>
                     </li>
                 @endif
             </ul>
