ab3a611e8eaeb6495da31c0936609870ceda1f50
Emmanuel Evance <emmanuel@irabu.co.tz>
2017-02-16 13:02:08 +0300

    created a checkConstraint method

--- app/Repositories/Capture/Capture.php
+++ app/Repositories/Capture/Capture.php
@@ -1,10 +1,14 @@
 <?php
 namespace Repositories\Capture;
 
+use App\Models\Teacher;
+use App\Models\Score;
+
 class Capture
 {
-	public function test()
+	public function checkConstraint($id)
 	{
-		return 'test';
+		$teacher = Teacher::subject()->where('id',$id)->firstOrFail();
+		$subject = Score::where('subject_id', $id)->firstOrFail();
 	}
 }
