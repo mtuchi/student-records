98efc9db855ca50a4a078320d060d56bfc804688
Emmanuel Evance <emmanuel@irabu.co.tz>
2017-02-23 14:36:54 +0300

    created Permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+		protected $fillable = ['name'];
+
+    public function roles()
+		{
+			return $this->belongsToMany(Role::class,'permission_role');
+		}
+}
