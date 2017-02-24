7b44c0a44acbd9edcebc3b16a14c82fd92056d99
Emmanuel Evance <emmanuel@irabu.co.tz>
2017-02-24 17:46:06 +0300

    created SendActivationEmail and ResendActivationEmail

---
+++ app/Mail/ResendActivationToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\ActivationToken;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ResendActivationToken extends Mailable
+{
+    use Queueable, SerializesModels;
+
+		public $token;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(ActivationToken $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Resended Activation Email')->view('email.auth.activation');
+    }
+}
