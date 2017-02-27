0b3e8b437025e18bb84c21774c24f68b21d5cd1b
Emmanuel Evance <emmanuel@irabu.co.tz>
2017-02-27 10:05:08 +0300

    commented all other seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,17 +14,17 @@ class DatabaseSeeder extends Seeder
 			$this->call(RolesTableSeeder::class);
       $this->call(PermissionsTableSeeder::class);
       // $this->call(UsersTableSeeder::class);
-      $this->call(StudentsTableSeeder::class);
-      $this->call(SubjectsTableSeeder::class);
-      $this->call(GradesTableSeeder::class);
-      $this->call(QuartersTableSeeder::class);
-      $this->call(ScoresTableSeeder::class);
-      $this->call(StudentSubjectTableSeeder::class);
-      $this->call(AttendancesTableSeeder::class);
-      $this->call(TeachersTableSeeder::class);
-      $this->call(GradeStudentTableSeeder::class);
-      $this->call(GradeSubjectTableSeeder::class);
-      $this->call(GradeQuarterTableSeeder::class);
-      $this->call(MonthsTableSeeder::class);
+      // $this->call(StudentsTableSeeder::class);
+      // $this->call(SubjectsTableSeeder::class);
+      // $this->call(GradesTableSeeder::class);
+      // $this->call(QuartersTableSeeder::class);
+      // $this->call(ScoresTableSeeder::class);
+      // $this->call(StudentSubjectTableSeeder::class);
+      // $this->call(AttendancesTableSeeder::class);
+      // $this->call(TeachersTableSeeder::class);
+      // $this->call(GradeStudentTableSeeder::class);
+      // $this->call(GradeSubjectTableSeeder::class);
+      // $this->call(GradeQuarterTableSeeder::class);
+      // $this->call(MonthsTableSeeder::class);
     }
 }
